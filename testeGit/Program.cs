7be588ca98b9e3ce83d9af2788f49fa0a30aa107
Rafael Lima <rafael_weblima@hotmail.com>
2020-11-01 22:09:29 -0300

    Alteracao de codigo

--- testeGit/Program.cs
+++ testeGit/Program.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
 
 namespace testeGit {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia, Qual seu nome?");
+            string entrada = Console.ReadLine();
+            Console.WriteLine("");
+            Console.WriteLine("Oi " + entrada + " Seja bem vindo!");
         }
     }
 }

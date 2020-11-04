b4221d18c95eb29ce54a76018b10151016f63902
Rafael Lima <rafael_weblima@hotmail.com>
2020-11-04 16:21:26 -0300

    Quebra de linha no final

--- testeGit/Program.cs
+++ testeGit/Program.cs
@@ -8,6 +8,14 @@ namespace testeGit {
             string entrada = Console.ReadLine();
             Console.WriteLine("");
             Console.WriteLine("Oi " + entrada + " Seja bem vindo!");
+
+            for (int i=0; i < 10; i++) {
+
+                Console.WriteLine("Valor i == " + i);
+
+            }
+
+            Console.WriteLine("");
         }
     }
 }
